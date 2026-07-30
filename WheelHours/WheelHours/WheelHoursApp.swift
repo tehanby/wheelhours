5785a888e2a589471b2de483dbc6dab5dd130de0
Tim Hanby <tim.hanby@resultstack.com>
2026-07-30 19:42:03 -0400

    xcode init

---
+++ WheelHours/WheelHours/WheelHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelHoursApp.swift
+//  WheelHours
+//
+//  Created by Tim Hanby on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct WheelHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
