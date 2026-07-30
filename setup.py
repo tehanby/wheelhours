12170743ea468af5ab2a248977697d0845911c69
DESKTOP-766Q7UK\timot <tim.hanby@resultstack.com>
2026-07-30 19:57:32 -0400

    feat: git checkout wheel hours and do the initial push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WheelHours',
+    version='0.1',
+    packages=find_packages(),
+    description='A project for managing wheel hours.',
+    author='Your Name',
+    author_email='your.email@example.com',
+)
