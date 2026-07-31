83c431a8e1d95d1b0c85eb69fe7f265615845336
Timothy Hanby <timothy.hanby@gmail.com>
2026-07-30 20:03:40 -0400

    feat: Build Failed (#13)
    Co-authored-by: DESKTOP-766Q7UK\timot <tim.hanby@resultstack.com>

---
+++ build/scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Remove previous build artifacts
+rm -rf /Users/timhanby/Library/Developer/Xcode/DerivedData/WheelHours-fksvjsapwinzavacxyucnezsdgop/Build/Products/Debug-iphonesimulator/WheelHours.app/
